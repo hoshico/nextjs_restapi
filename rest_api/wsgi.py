38532d6f4a3e0dc7970e8ec5122d71557fc36c8b
hoshico <melancholinista.yasu@gmail.com>
2022-03-13 14:55:12 +0900

    initial commit to heroku

--- rest_api/wsgi.py
+++ rest_api/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+from django.core.wsgi import get_wsgi_app
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_api.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
